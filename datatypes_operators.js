ed4e29b7ce57824435b4b97ff3727635151b1c22
Britney44 <mmetjabritney@gmail.com>
2026-05-26 19:49:45 +0200

    Adding the datatypes file exercise

---
+++ datatypes_operators.js
@@ -0,0 +1,21 @@
+let num1 =10;
+let num2 = 14.59;
+
+let addition = num1 + 15;
+let subtraction = num1 - 3;
+let multiplication = num1 * 100;
+let division = num1 * 2;
+let modulus = num1 % 3;
+let exponentiation = num1 ** 3;
+
+console.log("Addition :",addition);
+console.log("Subtraction :", subtraction);
+console.log("Multiplication :",multiplication);
+console.log("Division :",division);
+console.log("Modulus : ", modulus);
+console.log("Exponential :", exponentiation);
+
+let y = 12;
+let x = 8;
+
+
