d189c8095580ba33071c10453234ec9c05a23843
Britney44 <mmetjabritney@gmail.com>
2026-05-26 16:41:33 +0200

    Adding the variables file

---
+++ variables.js
@@ -0,0 +1,26 @@
+const name = "Britney";
+let age = 22;
+
+let isStudent = true;
+
+console.log("Name :", name);
+console.log("My age ", age);
+console.log("Student :", isStudent);
+
+let favoriteColour = "Blue";
+console.log("My favourite colour is ", favoriteColour);
+favoriteColour = "Pink";
+console.log("My favourite colour is ", favoriteColour);
+
+let num1 = 2;
+let num2= 3;
+let sum = num1 + num2;
+let difference = num2 -num1 ;
+let product = num1* num2;
+let quotient = num2 / num1 ;
+
+
+console.log ("The sum of the 2 numbers is ",sum);
+console.log("The difference between the two numbers is ",difference);
+console.log("The product of the numbers is ",product);
+console.log("The quotient of the two numbers is ",quotient);
